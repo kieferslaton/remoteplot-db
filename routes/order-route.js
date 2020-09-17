eabe5ecaee538f0d6e9c9d8dafec37df41e361da
Kiefer Slaton <kieferslaton@Kiefers-MacBook-Pro.local>
2020-09-17 15:34:18 -0500

    Backend Updates

--- routes/order-route.js
+++ routes/order-route.js
@@ -9,6 +9,16 @@ router.route('/:id').get((req, res) => {
     Order.findById(req.params.id).then(data => res.json(data)).catch(err => res.status(400).json(err))
 })
 
+router.route('/update/:id').post((req, res) => {
+    Order.findById(req.params.id).then(order => {
+        order.orderNumber = req.body.orderNumber;
+        order.billingDetails = req.body.billingDetails;
+        order.ship = req.body.ship;
+
+        order.save().then(() => res.json("Order updated")).catch(err => res.status(400).json(err))
+    }).catch(err => res.status(400).json(err))
+})
+
 router.route('/new').post((req, res) => {
     const orderNumber = req.body.orderNumber
     const ship = req.body.ship
